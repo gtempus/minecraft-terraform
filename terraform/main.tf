7921f6f37309a1241a2c2902cf74086ccff5bc16
gtempus <gary.tempus.jr@gmail.com>
2023-12-24 12:29:44 -0500

    chore(terraform): attach role for session manager

--- terraform/main.tf
+++ terraform/main.tf
@@ -316,33 +316,6 @@ resource "aws_iam_role_policy_attachment" "s3_read_policy_attachment" {
 ###################
 # Instance Connect
 ###################
-resource "aws_iam_role" "ec2_instance_connect_role" {
-  name = "ec2_instance_connect_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Action = "sts:AssumeRole",
-        Effect = "Allow",
-        Principal = {
-          Service = "ec2.amazonaws.com"
-        }
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ssm_core" {
-  role       = aws_iam_role.ec2_instance_connect_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-}
-
-resource "aws_iam_instance_profile" "ec2_instance_connect_profile" {
-  name = "ec2_instance_connect_profile"
-  role = aws_iam_role.ec2_instance_connect_role.name
-}
-
 resource "aws_iam_policy" "ec2_instance_connect_policy" {
   name        = "ec2_instance_connect_policy"
   description = "Policy to allow EC2 Instance Connect"
