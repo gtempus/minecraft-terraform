11ae98c5029af6d7ca020c4188c2626d1f62996d
gtempus <gary.tempus.jr@gmail.com>
2023-12-23 20:02:41 -0500

    chore(terraform): create bucket for ansible
    https://github.com/marketplace/actions/run-ansible-playbook

--- terraform/main.tf
+++ terraform/main.tf
@@ -265,3 +265,36 @@ resource "aws_iam_role_policy_attachment" "secrets_manager_access" {
   role       = aws_iam_role.lambda_role.name
 }
 
+####################################################
+# Create the S3 Bucket for Minecraft Server Ansible
+####################################################
+
+resource "aws_s3_bucket" "gtempus_minecraft_server_ansible" {
+  bucket = "gtempus-minecraft-server-ansible"
+}
+
+# IAM Policy for S3 Bucket Access
+resource "aws_iam_policy" "s3_read_policy" {
+  name        = "s3ReadPolicy"
+  description = "Policy to allow EC2 instance to read specific S3 bucket"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action   = ["s3:GetObject", "s3:ListBucket"],
+        Effect   = "Allow",
+        Resource = [
+          aws_s3_bucket.gtempus_minecraft_server_ansible.arn,
+          "${aws_s3_bucket.gtempus_minecraft_server_ansible.arn}/*"
+        ],
+      },
+    ],
+  })
+}
+
+# Attach the Policy to the IAM Role
+resource "aws_iam_role_policy_attachment" "s3_read_policy_attachment" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = aws_iam_policy.s3_read_policy.arn
+}
